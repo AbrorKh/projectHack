773c1ff706828157f1e423a565f1a4369c9711ba
Maksym9669 <maksym.sychov14@gmail.com>
2018-11-02 23:14:25 -0700

    Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import logo1 from "./logo.png";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <header className="App-header">
+          <img src={logo1} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
